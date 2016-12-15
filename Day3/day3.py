839afebb44d14db3f8da2c31cfd91b2e1180069a
Clement <clan@morphodetection.com>
2016-12-14 16:00:29 -0800

    Day3 done, Day4 prompt and input

--- Day3/day3.py
+++ Day3/day3.py
@@ -13,18 +13,22 @@ def do_sum(sides, sum_set):
 def validate_triangle(triangle):
     triangle = [int(x) for x in triangle]
     sides = set([0, 1, 2])
-    for i in range(0, 3):
-        s = sides - set([i])
-        if (triangle[i] > do_sum(triangle, s)):
-            return False
-    return True
+    if (triangle[0] < triangle[1] + triangle[2] and
+        triangle[1] < triangle[0] + triangle[2] and 
+        triangle[2] < triangle[0] + triangle[1]):
+        return True
+#    for i in range(0, 3):
+#        s = sides - set([i])
+#        if (triangle[i] > do_sum(triangle, s)):
+#            return False
+    return False
 
 def main():
     count = 0
     triangles = get_inputs()
     print "Total triangles: ", len(triangles)
     for triangle in get_inputs():
-        if validate_triangle(triangle):
+        if validate_triangle([x.strip() for x in triangle]):
             count = count + 1
     print count
 
