c98f20c0bb6c2cf3e4983a838e8327e080bcc1a6
Clement <clan@morphodetection.com>
2016-12-19 17:05:40 -0800

    day8, 2 stars

--- Day8/day8.py
+++ Day8/day8.py
@@ -12,8 +12,13 @@ class LCD(object):
             [False]*50,
             [False]*50]
 
-    def __init__(self):
+    def __init__(self, rows, cols):
         print("Initializing array of %d rows by %d columns" %(len(self.display), len(self.display[0])))
+        self.display = []
+        self.rows = rows
+        self.cols = cols
+        for i in range(rows):
+            self.display.append([False]*cols)
 
     def get_lit(self):
         count = 0
@@ -24,7 +29,7 @@ class LCD(object):
     def __str__(self):
         d = ""
         for i in self.display:
-            d = d + " ".join(['T' if x else 'F' for x in i]) + "\n"
+            d = d + " ".join(['T' if x else ' ' for x in i]) + "\n"
         return d
 
     def rect(self, A, B):
@@ -32,7 +37,6 @@ class LCD(object):
         print("Turning on rectangle %d by %d" %(A, B))
         for i in range(B):
             self.display[i][:A] = [True]*A
-            print(self.display[i][:A])
 
     def rotate_row(self, A, B):
         """
@@ -40,19 +44,36 @@ class LCD(object):
         Pixels that would fall off the right end appear at the left end of the row.
         """
         print("Shifting row %d by %d pixels" %(A, B))
-        self.display[A] = self.display[A][len(self.display[A])-B-1:] + self.display[A][:len(self.display[A])-B-1]
+        self.display[A] = self.display[A][len(self.display[A])-B:] + self.display[A][:len(self.display[A])-B]
 
     def rotate_col(self, A, B):
         """ Shifts column A down by B pixels """
-        
+        print("Shifting column %d by %d pixels" %(A, B))
+        # I think we could probably use zip for this?
+        o_col = [x[A] for x in self.display]
+        o_col = o_col[len(o_col)-B:] + o_col[:len(o_col)-B]
+        for i in range(self.rows):
+            self.display[i][A] = o_col[i]
+
+def process_input():
+    with open('input.txt') as f:
+        return [x.strip() for x in f.readlines()]
 
 def main():
-    lcd = LCD();
-    lcd.rect(3, 2)
+    lcd = LCD(6,50)
+    for instr in process_input():
+        if 'rect' in instr:
+            instr = instr.split()[-1].split('x')
+            lcd.rect(int(instr[0]),int(instr[1]))
+        else:
+            a = int(instr[instr.find('=')+1:].split()[0])
+            b = int(instr.split(' by ')[-1])
+            if 'row' in instr:
+                lcd.rotate_row(a, b)
+            elif 'column' in instr:
+                lcd.rotate_col(a, b)
+    print("Number of LEDs lit: ", lcd.get_lit())
     print(lcd)
-    lcd.rotate_row(0, 4)
-    print(lcd)
-    print("Number of pixels lit: ",lcd.get_lit())
 
 if __name__ == "__main__":
     main()
